b1422c7cf145cd60cd64c4e1be0692ba1d1dee12
Tyler Buchheim <tbuchhei@alumni.nd.edu>
2017-07-16 15:05:54 -0500

    add Menu module

--- bin/todo.rb
+++ bin/todo.rb
@@ -5,11 +5,16 @@ require_relative "../lib/modules"
 require "pry"
 
 if __FILE__ == $PROGRAM_NAME
-  my_list = List.new
-  my_task1 = Task.new("walk the dogs")
-  my_task2 = Task.new("code things")
+  include Menu
 
-  my_list.add(my_task1)
-  my_list.add(my_task2)
-  my_list.show
+  #my_list = List.new
+  #my_task1 = Task.new("walk the dogs")
+  #my_task2 = Task.new("code things")
+
+  #my_list.add(my_task1)
+  #my_list.add(my_task2)
+  #my_list.show
+
+  Menu.show
+
 end
---
+++ lib/modules.rb
@@ -0,0 +1,13 @@
+module Menu
+
+  def menu
+    puts "Welcome to the Spiffy To-Do List CLI!"
+    puts "Here are your current options:"
+    puts "1) Add\n2) Show\nQ) Quit"
+  end
+
+  def show
+    menu
+  end
+
+end
