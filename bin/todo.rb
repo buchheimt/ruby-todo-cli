c2507ea78f7c6059ce88417d9a60b4c1fb8c8e86
Tyler Buchheim <tbuchhei@alumni.nd.edu>
2017-07-16 10:46:20 -0500

    add reader and test Task class

--- bin/todo.rb
+++ bin/todo.rb
@@ -6,4 +6,6 @@ require "pry"
 if __FILE__ == $PROGRAM_NAME
   my_list = List.new
   puts "You have created a new list"
+  my_task = Task.new("walk the dogs")
+  puts my_task.description
 end
--- lib/classes.rb
+++ lib/classes.rb
@@ -4,8 +4,8 @@ end
 
 class Task
   attr_reader :description
-  def initialize(item)
-    @item = item
+  def initialize(description)
+    @description = description
   end
 
 
